5a2d77a50cb273cfdb4b700f4dd49e4285e8470f
Edgar <edgarignite1704@gmail.com>
2022-04-17 02:18:39 +0700

    update LoginGUI

--- src/_Entry/EntryMain.java
+++ src/_Entry/EntryMain.java
@@ -1,12 +1,14 @@
 package _Entry;
 
+import GUI.LoginGUI;
+
 import javax.swing.*;
 
 public class EntryMain {
     public static void main(String[] args) {
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-
+            new LoginGUI(new JFrame(), true).setVisible(true);
         } catch(Exception ex) {
             ex.printStackTrace();
         }
